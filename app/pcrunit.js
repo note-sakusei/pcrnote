785523256f7efa7312f001de3b467e169f6f8e24
note-sakusei <note-sakusei@undefined>
2024-10-01 14:44:58 +0900

    ノゾミ(アルケミスト)を追加

--- app/pcrunit.js
+++ app/pcrunit.js
@@ -234,6 +234,7 @@ pcrunit.PC_UNIT_INFO_LIST = [
     ['io', 'イオ', './img/arena/イオ.jpg', true],
     ['io_summer', 'イオ(サマー)', './img/arena/イオ(サマー).jpg', true],
     ['suzume', 'スズメ', './img/arena/スズメ.jpg', true],
+    ['nozomi_alchemist', 'ノゾミ(アルケミスト)', './img/arena/ノゾミ(アルケミスト).jpg', true],
     ['suzume_newyear', 'スズメ(ニューイヤー)', './img/arena/スズメ(ニューイヤー).jpg', true],
     ['creditta_christmas', 'クレジッタ(クリスマス)', './img/arena/クレジッタ(クリスマス).jpg', true],
     ['emilia', 'エミリア', './img/arena/エミリア.jpg', true],
