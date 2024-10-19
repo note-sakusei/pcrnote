181adffb30813951e55f0fe217fa8d1997f64008
note-sakusei <note-sakusei@undefined>
2024-10-19 22:28:33 +0900

    ヴルムを追加

--- app/pcrunit.js
+++ app/pcrunit.js
@@ -225,6 +225,7 @@ pcrunit.PC_UNIT_INFO_LIST = [
     ['maho_dreampark', 'マホ(ドリームパーク)', './img/arena/マホ(ドリームパーク).jpg', true],
     ['suzuna', 'スズナ', './img/arena/スズナ.jpg', true],
     ['suzuna_summer', 'スズナ(サマー)', './img/arena/スズナ(サマー).jpg', true],
+    ['wurm', 'ヴルム', './img/arena/ヴルム.jpg', true],
     ['homare_newyear', 'ホマレ(ニューイヤー)', './img/arena/ホマレ(ニューイヤー).jpg', true],
     ['yuni_winter', 'ユニ(ウィンター)', './img/arena/ユニ(ウィンター).jpg', true],
     ['homare_summer', 'ホマレ(サマー)', './img/arena/ホマレ(サマー).jpg', true],
